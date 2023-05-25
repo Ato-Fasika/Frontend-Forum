6fe5d6eb49655eda9e6a286d0e8ee04f088a9d38
Fasika Mulugeta <115148456+Fasika-MK@users.noreply.github.com>
2023-05-26 00:32:34 +0300

    1st-push

---
+++ src/Components/signUpPage/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './About.css'
+function About() {
+  return (
+    <div className='about'>
+        <h1>ABOUT</h1>
+        <h1>EVANGADI NETWORKS</h1>
+        <p>No matter what stage of life you are in, whether you are just starting elementary school or being promoted to CEO of a Fortune 500 company, you have much to offer to those who are trying to follow in your footsteps.</p>
+        
+        <p>Wheather you are willing to share your knowledge or you are just looking to meet mentors of your own, please start by joining the network here.</p>
+        <button>How it works</button>
+
+    </div>
+  )
+}
+
+export default About
